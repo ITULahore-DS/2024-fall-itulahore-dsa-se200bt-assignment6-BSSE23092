caf90510d650fa40fb82ba693ae7b11f904d2612
fatima faisal 92 <bsse23092@itu.edu.pk>
2024-09-30 20:09:46 +0500

    done

--- functions.cpp
+++ functions.cpp
@@ -1,47 +1,57 @@
 #include "functions.h"
 
+
+// constructor
 Node::Node(int val)
 {
     data = val;
     next = nullptr;
 }
 
+// destructor
 Node::~Node()
 {
     cout << "\n Destructor of Node ";
 }
 
+// set a node to next
 void Node::setNext(Node *nextNode)
 {
     next = nextNode;
 }
 
+// get next of a node
 Node *Node::getNext()
 {
     return next;
 }
 
+// set data in a node
 void Node::setData(int dataValue)
 {
     data = dataValue;
 }
 
+// get data of a node
 int Node::getData()
 {
     return data;
 }
 
+// constructor of stack
 Stack::Stack()
 {
     top = nullptr;
     count = 0;
 }
 
+// destructor
 Stack::~Stack()
 {
     // cout << " \nDestructor of Stack ";
 }
 
+// check stack is empty 
 bool Stack::isEmpty()
 {
     if (top == nullptr)
@@ -51,22 +61,24 @@ bool Stack::isEmpty()
     return false;
 }
 
+// puch a value in a stack
 void Stack::push(int data)
 {
     Node *new_node = new Node(data);
     if (isEmpty())
     {
         top = new_node;
-        count++;
+        count++;  //update count
     }
     else
     {
-        new_node->setNext(top);
-        top = new_node;
+        new_node->setNext(top);     
+        top = new_node;   //update the top
         count++;
     }
 }
 
+// remove the top 
 void Stack::pop()
 {
     if (isEmpty())
@@ -74,11 +86,13 @@ void Stack::pop()
         return;
     }
     Node *temp = top;
-    top = top->getNext();
+    top = top->getNext();    //update top
     delete temp;
     count--;
 }
 
+
+// return peak of the stack
 int Stack::peek()
 {
     if (isEmpty())
@@ -88,11 +102,13 @@ int Stack::peek()
     return top->getData();
 }
 
+// return the size of stack
 int Stack::size()
 {
     return count;
 }
 
+// clear the stack
 void Stack::clear()
 {
     while (!isEmpty())
@@ -101,6 +117,7 @@ void Stack::clear()
     }
 }
 
+// print the stack
 void Stack::printStack()
 {
     Node *temp = top;
@@ -111,17 +128,20 @@ void Stack::printStack()
     }
 }
 
+// constructor of queue
 Queue::Queue()
 {
     top = nullptr;
     count = 0;
 }
 
+// destructor
 Queue::~Queue()
 {
     // cout << " \nDestructor of Stack ";
 }
 
+// check queue is empty 
 bool Queue::isEmpty()
 {
     if (top == nullptr)
@@ -131,6 +151,7 @@ bool Queue::isEmpty()
     return false;
 }
 
+// puch a value in a queue
 void Queue::enqueue(int data)
 {
     Node *new_node = new Node(data);
@@ -151,6 +172,7 @@ void Queue::enqueue(int data)
     }
 }
 
+// remove the top 
 void Queue::dequeue()
 {
     if (isEmpty())
@@ -163,6 +185,7 @@ void Queue::dequeue()
     count--;
 }
 
+// return peak of the queue
 int Queue::peek()
 {
     if (isEmpty())
@@ -172,11 +195,13 @@ int Queue::peek()
     return top->getData();
 }
 
+// return the size of queue
 int Queue::size()
 {
     return count;
 }
 
+// clear the queue
 void Queue::clear()
 {
     while (!isEmpty())
@@ -185,6 +210,7 @@ void Queue::clear()
     }
 }
 
+// print the queue
 void Queue::printQueue()
 {
     Node *temp = top;
